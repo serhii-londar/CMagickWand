3066b85dfa8676ef789a5af1403acec851fec12b
Serhii Londar <serhii.londar@gmail.com>
2017-09-01 18:38:19 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "MagickWand",
+    targets: [
+        Target(name: "MagickWand", dependencies: [])
+    ],
+    dependencies: []
+)
