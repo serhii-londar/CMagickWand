240d489ba0e97ce5ce4484d5c8cbb8d6254479c2
Serhii Londar <serhii.londar@gmail.com>
2017-10-23 23:38:23 +0300

    Add header

--- Package.swift
+++ Package.swift
@@ -1,7 +1,10 @@
-// swift-tools-version:3.1
-
 import PackageDescription
 
 let package = Package(
-    name: "CMagickWand"
+    name: "CMagickWand",
+    pkgConfig: "magickwand"
+//    providers: [
+//        .brew("magickwand@7"),
+//        .apt("magickwand@7")
+//    ]
 )
